655499db5ea27e43998820c88996b7b95d8eba1d
eyal <eyalrev@gmail.com>
2018-05-18 22:36:59 +0300

    minor changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const port = process.env.port || 3000;
 
+//console.log(port);
 http.createServer((req,res)=>{
     res.end('This is a nodejs test...');
 }).listen(port);
