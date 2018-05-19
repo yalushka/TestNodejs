6badf13680c4d1b7544bb8422a1f5fa7f3cb5568
eyal <eyalrev@gmail.com>
2018-05-19 10:08:54 +0300

    changing ip port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
-const port = process.env.port || 3000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 //console.log(port);
 http.createServer((req,res)=>{
     res.end('This is a nodejs test...');
-}).listen(port);
+}).listen(port,serverIP);
+
 
