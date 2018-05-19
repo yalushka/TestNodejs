334b482ca0ef15607e71c0faf2bdd14d254d062e
eyal <eyalrev@gmail.com>
2018-05-19 12:03:46 +0300

    Bug Fix

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',;
 
 //console.log(port);
 http.createServer((req,res)=>{
-    res.end('This is a nodejs test...');
-}).listen(port,serverIP);
+    res.end('a very simple nodejs test...');
+}).listen(port,ip);
 
 
