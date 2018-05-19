1fbed6edd15338472fe828e9950c09d1142e0987
eyal <eyalrev@gmail.com>
2018-05-19 12:07:59 +0300

    small fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 //var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',;
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 //console.log(port);
 http.createServer((req,res)=>{
